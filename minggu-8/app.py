9d7b43abb5a880c913787a41ed945391cc546ec3
nuralifah28 <nuralifah101@gmail.com>
2019-12-03 17:16:47 +0700

    laporan pertemuan8

---
+++ minggu-8/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+
+app = Flask(_name_)
+
+@app.route("/")
+def hello();
+    return "Latihan membuat aplikasi menggunakan phyton+flask pada Docker!!"
+
+
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT",500))
+    app.run(debug=True,host='0.0.0.0',port=port)
